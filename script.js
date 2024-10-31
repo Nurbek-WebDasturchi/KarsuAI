c9eeb8369b6ed18a00872f02a36a228d52a13cb8
Nurbek-WebDasturchi <ahmedovnurbek26042005@gmail.com>
2024-11-01 00:05:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-            // Sahifa yuklanganda musiqa ijro etiladi
-            window.addEventListener("load", function () {
-                var audio = document.getElementById("background-music");
-                audio.play();
-                audio.muted = false;
 
-                // 23 soniyadan keyin ovozni pasaytirishni boshlaymiz
-                setTimeout(function () {
-                    var fadeOutInterval = setInterval(function () {
-                        if (audio.volume > 0.05) {
-                            audio.volume -= 0.05; // Ovozini asta-sekin pasaytirish
-                        } else {
-                            audio.volume = 0; // Ovoz butunlay pasaydi
-                            clearInterval(fadeOutInterval); // Intervalni to'xtatish
-                            audio.pause(); // Audioni to'xtatish
-                            audio.currentTime = 0; // Musiqani boshidan Batafsil uchun
-                        }
-                    }, 300); // Har 300 millisekundda ovoz pasayadi
-                }, 23000); // 23 soniyadan keyin fade-out boshlanadi
-            });
             const boxes = document.querySelectorAll(".box");
 
             boxes.forEach((box) => {
